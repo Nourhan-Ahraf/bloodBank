4a7798f5ccffeaeaccb360dd2f2fe306e66a5917
Nourhan-Ahraf <nouraashraf403@gmail.com>
2022-03-06 01:12:38 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*function sum (num1,num2){
+    var result = num1 + num2;
+    return result;
+}
+function calcAvg(sumNumbers){
+    console.log(sumNumbers/2);
+}
+var number1 = Number(window.prompt("enter number 1"))
+var number2 = Number(window.prompt("enter number 2"))
+ var sumResult = sum (number1 , number2);
+ calcAvg(sumResult);*/
+
+ 
+ function sayhello(){
+    var fisrtNme = document.getElementById("firstName").value
+    document.getElementById("demo").innerHTML=("Hello "+ fisrtNme)
+ 
+}
