cdde997293e646bd1d45fb68fcb8eb6dbda39b6e
bilt3jy <100893271+bilt3jy@users.noreply.github.com>
2022-03-06 21:27:46 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,7 +12,15 @@ var number2 = Number(window.prompt("enter number 2"))
 
  
  function sayhello(){
-    var fisrtNme = document.getElementById("firstName").value
-    document.getElementById("demo").innerHTML=("Hello "+ fisrtNme)
- 
+    let firstNme = document.getElementById("firstName").value
+    let sectionPrint= document.getElementById("print")
+    if(!firstNme==""){
+
+sectionPrint.classList.remove('d-none')
+sectionPrint.classList.add('d-block')
+        document.getElementById("demo").innerHTML=("Hello "+ firstNme)
+        document.getElementById("printname").innerHTML=firstNme
+    }else{
+        alert('plez enter your name ')
+    }
 }
